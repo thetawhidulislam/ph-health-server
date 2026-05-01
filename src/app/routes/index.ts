d8dcb6cd4f26ebe424eed3604064ec0811e1cfd9
unknown <designwithtawhid@gmail.com>
2026-05-01 22:14:33 +0600

    added the file in index route

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -3,13 +3,18 @@ import { SpecialityRoutes } from "../modules/speciality/speciality.route";
 import { AuthRoutes } from "../modules/auth/auth.route";
 import { UserRouter } from "../modules/user/user.route";
 import { DoctorRouter } from "../modules/doctor/doctor.route";
+import { scheduleRoutes } from "../modules/schedule/schedule.route";
+import { DoctorScheduleRoutes } from "../modules/doctorSchedule/doctorSchedule.route";
+import { AppointmentRoutes } from "../modules/appointment/appointment.router";
 
 const router = Router();
-
 
 router.use("/auth", AuthRoutes);
 router.use("/specialties", SpecialityRoutes);
 router.use("/users", UserRouter);
 router.use("/doctors", DoctorRouter);
+router.use("/schedules", scheduleRoutes);
+router.use("/doctor-schedules", DoctorScheduleRoutes);
+router.use("/appointments", AppointmentRoutes);
 
 export const IndexRoutes = router;
