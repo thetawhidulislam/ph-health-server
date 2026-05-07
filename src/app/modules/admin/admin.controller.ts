bf53a18f053a399f95e617a464f0a618173a8921
unknown <designwithtawhid@gmail.com>
2026-05-07 16:55:33 +0600

    updatd change stutas and role update

--- src/app/modules/admin/admin.controller.ts
+++ src/app/modules/admin/admin.controller.ts
@@ -57,10 +57,39 @@ const deleteAdmin = catchAsync(async (req: Request, res: Response) => {
     data: result,
   });
 });
+const changeUserStatus = catchAsync(async (req: Request, res: Response) => {
+  const user = req.user as IRequestUser;
+  const payload = req.body;
+  const result = await AdminService.changeUserStatus(user, payload);
+  sendResponse(res, {
+    httpStatusCode: status.OK,
+    success: true,
+    message: "User status changed successfully",
+    data: result,
+  });
+});
+
+const changeUserRole = catchAsync(async (req: Request, res: Response) => {
+  const user = req.user;
+  const payload = req.body;
+  const result = await AdminService.changeUserRole(
+    user as IRequestUser,
+    payload,
+  );
+  sendResponse(res, {
+    httpStatusCode: status.OK,
+    success: true,
+    message: "User role changed successfully",
+    data: result,
+  });
+});
 
 export const AdminController = {
   getAllAdmins,
   updateAdmin,
   deleteAdmin,
   getAdminById,
+  changeUserStatus,
+  changeUserRole,
+  
 };
--- src/app/modules/admin/admin.route.ts
+++ src/app/modules/admin/admin.route.ts
@@ -25,5 +25,15 @@ router.patch(
   AdminController.updateAdmin,
 );
 router.delete("/:id", checkAuth(Role.SUPER_ADMIN), AdminController.deleteAdmin);
+router.patch(
+  "/change-user-status",
+  checkAuth(Role.SUPER_ADMIN, Role.ADMIN),
+  AdminController.changeUserStatus,
+);
+router.patch(
+  "/change-user-role",
+  checkAuth(Role.SUPER_ADMIN),
+  AdminController.changeUserRole,
+);
 
 export const AdminRoutes = router;
